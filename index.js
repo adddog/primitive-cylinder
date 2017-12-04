a2e6e25b3a79c046e50b59a34ae6cc47dac88e64
samelie <sam@stinkdigital.com>
2017-12-04 14:00:31 -0800

    use points

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function createCylinderMesh(
   radiusBottom,
   height,
   radialSegments,
-  heightSegments
+  heightSegments,
+  points,
 ) {
   var index = 0
   var indexOffset = 0
@@ -17,9 +18,9 @@ function createCylinderMesh(
     capCount++
   }
 
-  var vertexCount = ((radialSegments + 1) * (heightSegments + 1)) +
+  var vertexCount = ((radialSegments + 1) * (points.length + 1)) +
     ((radialSegments + 2) * capCount)
-  var cellCount = (radialSegments * heightSegments * 2) + (radialSegments * capCount)
+  var cellCount = (radialSegments * points.length * 2) + (radialSegments * capCount)
 
   var normals = new Array(vertexCount)
   var vertices = new Array(vertexCount)
@@ -29,9 +30,9 @@ function createCylinderMesh(
   var slope = (radiusBottom - radiusTop) / height
   var thetaLength = 2.0 * Math.PI
 
-  for (var y = 0; y <= heightSegments; y++) {
+  for (var y = 0; y <= points.length; y++) {
     var indexRow = []
-    var v = y / heightSegments
+    var v = points[y][1]//y / heightSegments
     var radius = v * (radiusBottom - radiusTop) + radiusTop
 
     for (var x = 0; x <= radialSegments; x++) {
@@ -39,7 +40,7 @@ function createCylinderMesh(
       var theta = u * thetaLength
       var sinTheta = Math.sin(theta)
       var cosTheta = Math.cos(theta)
-      vertices[index] = [radius * sinTheta, -v * height + (height / 2), radius * cosTheta]
+      vertices[index] = [radius * sinTheta, -v * height + (height / 2), radius * cosTheta * points[y][2]]
       normals[index] = [sinTheta, slope, cosTheta]
       uvs[index] = [u, 1 - v]
 
@@ -51,7 +52,7 @@ function createCylinderMesh(
   }
 
   for (var x = 0; x < radialSegments; x++) {
-    for (var y = 0; y < heightSegments; y++) {
+    for (var y = 0; y < points.length; y++) {
       var i1 = indexArray[y][x]
       var i2 = indexArray[y + 1][x]
       var i3 = indexArray[y + 1][x + 1]
